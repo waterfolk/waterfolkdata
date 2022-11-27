fdb2ced4b46bc8fb7a2beb5050f40d581f2f4c5d
stephen powers <st3powers@gmail.com>
2022-11-27 13:11:07 -0600

    updated filenames and archive

---
+++ archive/beforeupdate_2022Nov27/fetchlimnochem_boxauth.R
@@ -0,0 +1,30 @@
+#
+
+library(boxr)
+
+
+#=====================================
+# Authenticate Box
+# May need to update client secret periodically, or when Box app settings are changed (?) 
+
+box_auth(client_id = "7npn73lftgc6r5uzzejfdxkq02r6ah30", 
+    client_secret = "CsdTSbJpt07DkBOVfyrmBYFw4Sgw4jNh",
+    interactive=FALSE,cache=TRUE)
+
+#=====================================
+# Read in data and merge tables
+
+bottles0<-box_read(853293893514)
+bottles<-bottles0
+measurements0<-box_read(853292641714)
+measurements<-measurements0
+
+#PATH<-"https://raw.githubusercontent.com/waterfolk/waterfolkdata/main/bottles.csv"
+#bottles0 <- fread(PATH, header =  TRUE, sep = ',',stringsAsFactors=FALSE)
+
+#PATH<-"https://raw.githubusercontent.com/waterfolk/waterfolkdata/main/measurements.csv"
+#measurements0 <- fread(PATH, header =  TRUE, sep = ',',stringsAsFactors=FALSE)
+
+write.csv(bottles,"bottles.csv",row.names=FALSE)
+write.csv(measurements,"measurements.csv",row.names=FALSE)
+
